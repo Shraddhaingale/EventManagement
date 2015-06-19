793a5e61d0a21c7e4cc351fbdd9352147b1e673c
gaurav <gaurav@sancustechnologies.com>
2015-06-19 20:40:26 +0530

    Assignment without Ajax

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,8 @@
 class Event < ActiveRecord::Base
+
+
+validates :event_title , :presence =>"Please enter tittle"
+validates :event_start_date , :presence =>"Please enter tittle"
+validates :event_color , :presence =>"Please enter tittle"
+
 end
